3958f38b751a8698abf4c24887eba69286aee47f
shoddyshaman <djtiwari90@gmail.com>
2021-01-21 15:54:31 -0700

    finished lab react hooks

---
+++ app-1/src/Components/AddTodo.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const AddTodo = (props) => {
+    const [userInput, setUserInput] = useState('')
+
+    function handleToAddTodo(e){
+        e.preventDefault();
+        props.addTodo(userInput);
+        setUserInput('')
+    }
+
+    return (
+        <form onSubmit={handleToAddTodo}>
+            <input
+            placeholder="Add a Todo!"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            />
+            <button>Submit</button>
+        </form>
+    )
+}
+
+export default AddTodo;
--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { useState , useEffect} from 'react';
+import axios from 'axios';
+import ListItem from './Components/ListItem'
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([])
+
+  useEffect(() =>{
+    axios.get('https://pokeapi.co/api/v2/pokemon')
+    .then((res) => {
+      setList(res.data.results);
+    })
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {list.map((element, index) =>{
+        return <ListItem key={index} name={element.name} />
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
